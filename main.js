6b51ed276d6ea45651dbd32c76931423586acf90
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2022-02-23 12:25:04 -0500

    hooked up astrology javascript

--- main.js
+++ main.js
@@ -150,19 +150,150 @@ maulBtn.addEventListener('click', function(){
 xbutton.addEventListener('click', function(){
     popup_container.classList.remove('popup-visible');
     console.log('popup removed');
+    obiSound.pause();
+    quigonSound.pause();
+    threepioSound.pause();
+    yodaSound.pause();
+    winduSound.pause();
+    lukeSound.pause();
+    jabbaSound.pause();
+    vaderSound.pause();
+    sidiousSound.pause();
+    greviousSound.pause();
+    dookuSound.pause();
+    maulSound.pause();
+
 })
 
 
+// Calander date section
+const userSubmitBtn = document.getElementById('userSubmit');
+
+
+userSubmitBtn.addEventListener('click', function() {
+
+
+    const userBday = new Date(document.getElementById('bday').value);
+    //console.log(userBday);
+
+    const whichMonth = userBday.getUTCMonth() + 1;
+    //console.log(`Users month is: ${whichMonth}`);
+
+    const whichDayOfMonth = userBday.getUTCDate();
+    //console.log(`Users birth day is: ${whichDayOfMonth}`);
+
+    // Zodiac Algorithm
+    if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+        AstroSign = "Sidious";
+        popup_container.classList.add('popup-visible');
+        text.textContent= "Like Darth Sidious, you are patient, perseverant, \n\
+        and skilled in navigating emotional and political situations.";
+        sidiousSound.play();
+        image.src="images/sidious.jpg";
+        title.textContent= "Darth Sidious";
+
+    } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+    AstroSign = "Sag";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Dooku, you are alwys seeking knowledge, \n\
+    and enjoy chasing after intellectual and spiritual exploits.";
+    dookuSound.play();
+    image.src="images/dooku.jpg";
+    title.textContent= "Count Dooku";
+
+    } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+    AstroSign = "Sco";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Maul, you are elusive, mysterious, \n\
+    and who is driven by their emotions to cultivate sprawling wisdom.";
+    maulSound.play();
+    image.src="images/maul.jpg";
+    title.textContent= "Darth Maul";
+
+    } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+    AstroSign = "Lib";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Mace Windu, you are focused on balance and equilibrium, \n\
+    and strive to find harmony in every part of your life.";
+    winduSound.play();
+    image.src="images/windu.jpg";
+    title.textContent= "Mace Windu";
+
+    } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+    AstroSign = "Vir";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Obi Wan, you are logical, practical, and dutiful\n\
+     while always trying to improve the way you live your life.";
+    obiSound.play();
+    image.src="images/obiwan.jpg";
+    title.textContent= "Obi Wan Kenobi";
+
+    } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+    AstroSign = "Leo";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Grevious, you have a fiery flair for the dramatic, \n\
+    and love to bask in the spotlight.";
+    greviousSound.play();
+    image.src="images/grevious.jpg";
+    title.textContent= "General Grevious";
+
+    } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+    AstroSign = "Can";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Qui-Gon, you are a guarded, intuitive \n\
+    and have a strong connection between the material worlds.";
+    quigonSound.play();
+    image.src="images/quigon.jpg";
+    title.textContent= "Qui-Gon Jin";
+
+    } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+    AstroSign = "Gem";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like C3PO, you are quite a busy and sometimes \n\
+    erratic individual. But you are also full of pure curiosity.";
+    threepioSound.play();
+    image.src="images/threepio.jpg";
+    title.textContent= "C3PO";
+
+    } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+    AstroSign = "Tau";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Jabba, you are drawn to luxury, \n\
+    decadence and enjoy a life of relaxation.";
+    jabbaSound.play();
+    image.src="images/jabba.jpg";
+    title.textContent= "Jabba The Hutt";
+
+    } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+    AstroSign = "Ari";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Darth Vader, you are bold, ambitious, \n\
+    and enjoy diving headfirst into even the most challenging situations.";
+    vaderSound.play();
+    image.src="images/vader.jpg";
+    title.textContent= "Darth Vader";
+
+    } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+    AstroSign = "Pis";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Yoda, you are sensitive, wise beyond your years, \n\
+    and full of empathetic imagination.";
+    yodaSound.play();
+    image.src="images/yoda.jpg";
+    title.textContent= "Yoda";
+
+    } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+    AstroSign = "Aqu";
+    popup_container.classList.add('popup-visible');
+    text.textContent= "Like Luke, you are innovative, progressive, \n\
+    and always focused on making the world a better place.";
+    lukeSound.play();
+    image.src="images/luke.jpg";
+    title.textContent= "Luke Skywalker";
+    }
+
+    console.log(AstroSign);
+});
 
 
 
-
-// obiwanBtn.addEventListener('click', function() {userPicked('Obi wan');});
-
-// // create function userPicked();
-// function userPicked(person) {
-//     console.log(`The user picked ${person}`);
-// }
-// function userPicked(buttonName) {
-//     console.log('User picked ' + buttonName);
-// }
